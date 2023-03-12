80585990d00e75fcc907cae64cd71092970c135c
Kseniia Beznos <ksenia.beznos@yandex.ru>
2023-03-12 20:47:54 +0300

    Hotfix

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ function ImagePopup(props) {
 	return (
 		<div className={`popup popup_type_image ${props.isOpen ? 'popup_opened' : ''}`}>
 			<figure className='popup__container-image'>
-				<img className='popup__photo' src={props.card.link} onClick={props.isOpen} alt={props.card.name} />
+				<img className='popup__photo' src={props.card.link} alt={props.card.name} />
 				<figcaption className='popup__figcap'>{props.card.name}</figcaption>
 				<button
 					className='popup__close-icon popup__close-icon_type_image'
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
